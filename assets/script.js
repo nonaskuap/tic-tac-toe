e2a957bf974022317e289e9f03a56b8b4d97829c
meow roar <nonaskuap@gmail.com>
2023-10-15 01:50:59 -0700

    add eventListener to buttons

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+const player1 = 'x', player2 = 'o';
+let turn = 0;
+
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+const btn3 = document.querySelector('#btn3');
+
+const btn4 = document.querySelector('#btn4');
+const btn5 = document.querySelector('#btn5');
+const btn6 = document.querySelector('#btn6');
+
+const btn7 = document.querySelector('#btn7');
+const btn8 = document.querySelector('#btn8');
+const btn9 = document.querySelector('#btn9');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+btn1.addEventListener('click', () => check(1));
+btn2.addEventListener('click', () => check(2));
+btn3.addEventListener('click', () => check(3));
+
+btn4.addEventListener('click', () => check(4));
+btn5.addEventListener('click', () => check(5));
+btn6.addEventListener('click', () => check(6));
+
+btn7.addEventListener('click', () => check(7));
+btn8.addEventListener('click', () => check(8));
+btn9.addEventListener('click', () => check(9));
